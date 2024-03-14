54b264ef9ca707e26a1f8987a9a37f1aace1e9de
SUJITH KANNAN K H <67060938+Suji-2d@users.noreply.github.com>
2024-03-14 02:52:01 +0000

    Update and rename dataAPI.js to ConfigAPI.js

---
+++ question-app/src/api/ConfigAPI.js
@@ -0,0 +1,11 @@
+// Define your API URLs as constants. 
+//React components, you can import these URLs from here and use them when making API requests
+
+
+// Base URL of your API
+const BASE_URL = "https://example.com/api";
+
+// URLs for specific endpoints
+export const GET_RECORDS_API_URL = `${BASE_URL}/records`;
+export const POSTS_RECORD_API_URL = `${BASE_URL}/create-records`;
+export const PUT_RECORD_API_URL = `${BASE_URL}/update-records`;
